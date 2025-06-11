b4938f410c8f3b5134e48f578aab98fc50cbc560
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2025-06-11 14:09:03 +0700

    add column

---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First Flutter App' ,
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          backgroundColor: Colors.blue,
+        ),
+        body: Column(
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.amber,
+              child: Center(child: Text("Hallo"))
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.red,
+              child: Center(child: Text("Hallo"))
+            ),
+            Container(
+              width: 100,
+              height: 100,
+              color: Colors.blue,
+              child: Center(child: Text("Hallo"))
+            )
+          ],
+        )
+
+      )
+    );
+  }
+}
