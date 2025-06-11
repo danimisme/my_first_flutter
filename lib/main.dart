43f168167f2fca2ac745e39701e3122aacf3f93c
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2025-06-11 13:45:56 +0700

    update MyApp to use a network image instead of an asset image

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,12 @@ class MyApp extends StatelessWidget {
           // 1. Asset Image
           // --> gambar yang ada pada folder project
           // --> perlu daftar di pubspec.yaml
-          child: Image(image: AssetImage("assets/unnamed.png") ),
+          // child: Image(image: AssetImage("assets/unnamed.png") ),
+          // 2. Network Image
+          // --> gambar yang ada di internet
+             child: Image(
+              image: NetworkImage("https://images.unsplash.com/photo-1541823709867-1b206113eafd?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+              )
         )
       )
     );
