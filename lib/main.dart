d4fe909e383f8c042dd803f77ac8b6b067e8c341
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2025-06-11 17:11:46 +0700

    refactor MyApp to generate color boxes dynamically

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
+import "dart:math";
+
 import "package:flutter/material.dart";
 import "package:my_first_flutter/widgets/kotak_warna.dart";
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+
+   final List<KotakWarna> allItems = List.generate(10, (index) => KotakWarna(text: "Kotak ke-$index", warna: Color.fromARGB(
+    255,
+    200 + Random().nextInt(256), // Random red value
+    200 + Random().nextInt(256), // Random green value
+    200 + Random().nextInt(256)  // Random blue value
+   )));
 
   @override
   Widget build(BuildContext context) {
@@ -16,16 +24,7 @@ class MyApp extends StatelessWidget {
         body: SingleChildScrollView(
           scrollDirection: Axis.vertical,
           child: Column(
-            children: [
-              KotakWarna(text: "Merah", warna: Colors.red),
-              KotakWarna(text: "Kuning", warna: Colors.amber),
-              KotakWarna(text: "Biru", warna: Colors.blue),
-              KotakWarna(text: "Hijau", warna: Colors.green),
-              KotakWarna(text: "Ungu", warna: Colors.purple),
-              KotakWarna(text: "Coklat", warna: Colors.brown),
-              KotakWarna(text: "Hitam", warna: Colors.black),
-              KotakWarna(text: "Putih", warna: Colors.white),
-            ]
+            children: allItems
           ),
         ),
       )
