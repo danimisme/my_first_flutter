c25b763e5c89767e3fc92ca49e945013dfac49a8
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2025-06-12 10:13:12 +0700

    enhance app bar design

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,31 @@
-import "dart:math";
-
 import "package:flutter/material.dart";
-import "package:my_first_flutter/widgets/kotak_warna.dart";
 
-void main() {
+void main(){
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-   final List<KotakWarna> allItems = List.generate(10, (index) => KotakWarna(text: "Kotak ke-$index", warna: Color.fromARGB(
-    255,
-    200 + Random().nextInt(256), // Random red value
-    200 + Random().nextInt(256), // Random green value
-    200 + Random().nextInt(256)  // Random blue value
-   )));
-
-   List<Map<String, dynamic>> data = List.generate(10, (index) => {
-    "text": "Kontak ke-$index",
-    "warna": Color.fromARGB(
-        255,
-        100 + Random().nextInt(256), // Random red value
-        100 + Random().nextInt(256), // Random green value
-        100 + Random().nextInt(256)  // Random blue value
-      )
-    }
-  );
-   
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("My Application", style: TextStyle(color: Colors.white)), backgroundColor: Colors.blue),
-        body: GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
+        appBar: AppBar(
+          leading: FlutterLogo(size: 20),
+          backgroundColor: Colors.blue,
+          title: Text("AppBar"),
+          titleTextStyle: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
           ),
-          itemCount: 50,
-          itemBuilder: (context, index) => Container(
-            color: Color.fromARGB(255, 
-            60+ Random().nextInt(256),
-            60+ Random().nextInt(256),
-            60+ Random().nextInt(256)),
-          )
+          actions: [
+            IconButton(onPressed: (){print("Klik more");}, icon: Icon(Icons.more_vert , color: Colors.white)),
+          ],
         ),
       )
     );
   }
 }
-
