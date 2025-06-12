621f3b25a91c1e2dbbe6734c43bc531ee095227c
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2025-06-12 10:35:49 +0700

    add soal_2 widget with app bar and greeting text

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import "package:flutter/material.dart";
 import "package:my_first_flutter/widgeds/soal_1.dart";
+import "package:my_first_flutter/widgeds/soal_2.dart";
 
 void main(){
   runApp(MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: soal_1()
+      home: soal_2()
     );
   }
 }
