e069f661faaad9f1ba838384e418195f5c72c2f6
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2025-06-13 09:58:37 +0700

    use TextField and implement email/password fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,51 +10,84 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: HomePage()
     );
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({
+    super.key,
+  });
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool isHidden = true;
+  TextEditingController emailC = TextEditingController();
+  TextEditingController passwordC = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AppBar",
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-            )),
-            backgroundColor: Colors.blue,
+        title: Text("TextField Example"),
+        leading: FlutterLogo(size: 20),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: (){
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text("Hello", style: TextStyle(color: Colors.black)),
-                action: SnackBarAction(
-                  label: "Close",
-                  textColor: Colors.red,
-                  onPressed: () {
-                    print("Tidak Jadi hapus");
-                  },
-                ),
-                backgroundColor: Colors.amber,
-                duration: Duration(milliseconds: 100),
-                margin: EdgeInsets.all(20),
-                behavior: SnackBarBehavior.floating,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
+        child: ListView(
+          padding: const EdgeInsets.all(30),
+          children:[ 
+            TextField(
+            autocorrect: false,
+            controller: emailC,
+            keyboardType: TextInputType.emailAddress,
+            textInputAction: TextInputAction.next,
+            decoration: InputDecoration(
+              label: Text("Email"),
+              border: OutlineInputBorder(),
+              prefixIcon: Icon(Icons.email),
+            ),
+            ),
+            SizedBox(height: 20),
+            TextField(
+            autocorrect: false,
+            controller: passwordC,
+            obscureText: isHidden,
+            textInputAction: TextInputAction.done,
+            decoration: InputDecoration(
+              labelText: "Password",
+              border: OutlineInputBorder(),
+              prefixIcon: Icon(Icons.lock),
+              suffixIcon: IconButton(
+                onPressed: (){
+                  setState(() {
+                    isHidden = !isHidden;
+                  });
+                }, 
+                icon: Icon(
+                  isHidden ? Icons.visibility : Icons.visibility_off
+                )),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: (){
+              print("Email: ${emailC.text} , Password: ${passwordC.text}");
+            }, 
+            child: Text("Login"),
+            style: ElevatedButton.styleFrom(
+              minimumSize: Size.fromHeight(50),
+              foregroundColor: Colors.white,
+              backgroundColor: Colors.blue,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5)
               )
-            );
-          }, 
-          child: Text("Show Snackbar"))
-      )
+              ),
+            )
+          ]
+        ),
+      ),
     );
   }
 }
