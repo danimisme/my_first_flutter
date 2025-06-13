673ff7dd118e20112c77f76772e7408459c54e79
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2025-06-13 13:49:47 +0700

    Create dropdown

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
 void main() {
   runApp(MyApp());
@@ -10,84 +10,41 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage()
+      home: HomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({
-    super.key,
-  });
+class HomePage extends StatelessWidget {
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
+  final List data = [
+    "Data 1",
+    "Data 2",
+    "Data 3",
+    "Data 4",
+    "Data 5",
+  ];
 
-class _HomePageState extends State<HomePage> {
-  bool isHidden = true;
-  TextEditingController emailC = TextEditingController();
-  TextEditingController passwordC = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("TextField Example"),
-        leading: FlutterLogo(size: 20),
+        title: Text("Dropdown Example", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.teal,
       ),
       body: Center(
-        child: ListView(
-          padding: const EdgeInsets.all(30),
-          children:[ 
-            TextField(
-            autocorrect: false,
-            controller: emailC,
-            keyboardType: TextInputType.emailAddress,
-            textInputAction: TextInputAction.next,
-            decoration: InputDecoration(
-              label: Text("Email"),
-              border: OutlineInputBorder(),
-              prefixIcon: Icon(Icons.email),
-            ),
-            ),
-            SizedBox(height: 20),
-            TextField(
-            autocorrect: false,
-            controller: passwordC,
-            obscureText: isHidden,
-            textInputAction: TextInputAction.done,
-            decoration: InputDecoration(
-              labelText: "Password",
-              border: OutlineInputBorder(),
-              prefixIcon: Icon(Icons.lock),
-              suffixIcon: IconButton(
-                onPressed: (){
-                  setState(() {
-                    isHidden = !isHidden;
-                  });
-                }, 
-                icon: Icon(
-                  isHidden ? Icons.visibility : Icons.visibility_off
-                )),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(onPressed: (){
-              print("Email: ${emailC.text} , Password: ${passwordC.text}");
-            }, 
-            child: Text("Login"),
-            style: ElevatedButton.styleFrom(
-              minimumSize: Size.fromHeight(50),
-              foregroundColor: Colors.white,
-              backgroundColor: Colors.blue,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5)
-              )
-              ),
-            )
-          ]
-        ),
-      ),
+        child: Padding(
+          padding: EdgeInsets.all(30),
+            child: DropdownButton(
+              items: data.map((item) =>
+                DropdownMenuItem(
+                  child: Text("Pilihan: $item"),
+                  value: item,
+                  )).toList(), 
+                  onChanged: (value) {
+                    print(value);
+                  }),
+            )),
     );
   }
 }
