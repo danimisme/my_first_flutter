55283842057c1681e1137a1bdb58b13043e09cc4
Muhammad Subhan Ramdhani <subhan.ramdhani@tsmid.com>
2025-06-16 10:42:12 +0700

    Create navigation structure

--- lib/main.dart
+++ lib/main.dart
@@ -16,35 +16,87 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-
-  final List data = [
-    "Data 1",
-    "Data 2",
-    "Data 3",
-    "Data 4",
-    "Data 5",
-  ];
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dropdown Example", style: TextStyle(color: Colors.white)),
-        backgroundColor: Colors.teal,
+        title: Text("Home Page"),
       ),
       body: Center(
-        child: Padding(
-          padding: EdgeInsets.all(30),
-            child: DropdownButton(
-              items: data.map((item) =>
-                DropdownMenuItem(
-                  child: Text("Pilihan: $item"),
-                  value: item,
-                  )).toList(), 
-                  onChanged: (value) {
-                    print(value);
-                  }),
-            )),
+        child: ElevatedButton(
+          onPressed:  () {
+            Navigator.of(context).push(
+              MaterialPageRoute(builder: (context) => ProductPage())
+            );
+          }, 
+          child: Text("Next Page >>")
+          )
+      )
+    );
+  }
+}
+
+
+class ProductPage extends StatelessWidget {
+  const ProductPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Product Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Ini Halaman Produk"),
+            SizedBox(height: 20),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                spacing: 20,
+                children: [
+                  ElevatedButton(onPressed: () {
+                    Navigator.of(context).pop();
+                  }, child: Text("<<< Back")),
+                  ElevatedButton(onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => ProfilePage())
+                    );
+                  }, child: Text("Next >>>"),)
+                ],
+              )
+          ],
+        ),
+      )
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile Page"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Ini Halaman Profil'),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text("<<< Back")),
+          ],
+        )
+      ),
     );
   }
 }
